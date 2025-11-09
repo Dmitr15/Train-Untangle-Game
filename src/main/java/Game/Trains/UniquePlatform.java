83b821ecf26faeb2e0222b582a5f73464c9565de
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-11-09 17:18:10 +0300

    Added new type of platform

---
+++ src/main/java/Game/Trains/UniquePlatform.java
@@ -0,0 +1,110 @@
+//package Game.Trains;
+//
+//import Game.Field;
+//
+//import java.awt.geom.Point2D;
+//import java.util.List;
+//
+//public class UniquePlatform extends Platform{
+//
+//    private final int steps;
+//
+//    public UniquePlatform(Point2D position, List<Point2D> paths, Field field, int steps) {
+//        super(position, paths, field);
+//        this.steps = steps;
+//    }
+//
+//    public int getSteps() {
+//        return steps;
+//    }
+//}
+
+
+package Game.Trains;
+
+import Game.Field;
+import java.awt.geom.Point2D;
+import java.util.List;
+
+public class UniquePlatform extends Platform {
+    private final int maxSteps;
+    private int currentSteps;
+    private boolean isMoveable = true;
+    private Train pushingTrain;
+
+    public void setMoveable(boolean moveable) {
+        isMoveable = moveable;
+    }
+
+    public boolean isMoveable() {
+        return isMoveable;
+    }
+
+    public Train getPushingTrain() {
+        return pushingTrain;
+    }
+
+    public UniquePlatform(Point2D position, List<Point2D> paths, Field field, int steps) {
+        super(position, paths, field);
+        this.maxSteps = steps;
+        this.currentSteps = 0;
+        this.pushingTrain = null;
+    }
+
+    @Override
+    protected void performMovement(Point2D nextPosition) {
+        if (currentSteps < maxSteps) {
+              currentSteps++;
+
+            // Если это был первый шаг, запоминаем поезд и его исходную позицию
+            if (currentSteps == 1) {
+                for (abstractPlatform train : getField().getTrains()) {
+                    boolean isQ = train.getPosition().equals(position);
+                    if (train instanceof Train && isQ) {
+                        pushingTrain = (Train) train;
+                        break;
+                    }
+                }
+            }
+
+            super.performMovement(nextPosition);
+
+        } else {
+            this.isMoveable = false;
+            // Достигнут лимит шагов - вызываем столкновение для поезда
+            if (pushingTrain != null) {
+                pushingTrain.handleCollision();
+                pushingTrain = null;
+                currentSteps = 0;
+                this.setDirection(null);
+            }
+        }
+    }
+
+    @Override
+    protected void handleInvalidMovement() {
+        // При невалидном движении сбрасываем счетчик
+        currentSteps = 0;
+        pushingTrain = null;
+        this.isMoveable = false;
+        super.handleInvalidMovement();
+    }
+
+    @Override
+    protected void handleCollision() {
+        // При столкновении сбрасываем счетчик
+        currentSteps = 0;
+        pushingTrain = null;
+        //initialTrainPosition = null;
+        this.isMoveable = false;
+        super.handleCollision();
+    }
+
+    public int getSteps() {
+        return maxSteps;
+    }
+
+    public int getCurrentSteps() {
+        return currentSteps;
+    }
+}
